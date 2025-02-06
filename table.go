86f222f9eca0f60a96c04b9b42ba2dc2c06cb7fd
BatuhanAlun <22220030071@mersin.edu.tr>
2025-02-06 08:18:58 +0300

    AddColumn

--- table.go
+++ table.go
@@ -1,5 +1,10 @@
 package godb
 
+import (
+	"errors"
+	"log"
+)
+
 func CreateTable(tableName string) *Table {
 	return &Table{Name: tableName}
 }
@@ -13,9 +18,22 @@ func CreateColumn(cName, cType string, cMode ...string) Column {
 	if len(cMode) > 0 && cMode[0] == "PK" {
 		mode = "PK"
 	}
+
+	if cType != "int" && cType != "bool" && cType != "string" {
+		log.Fatal(errors.New("type needs to be int, bool or string"))
+	}
+
 	return Column{Name: cName, Type: cType, Mode: mode}
 }
 
 func (t *Table) AddRow(row *Row) {
 	t.Rows = append(t.Rows, row)
+}
+
+func (t *Table) AddColumn(col Column) {
+	for _, v := range t.Columns {
+		if v.Name == col.Name {
+			log.Fatal(errors.New("Column Names Needs to Be Unique"))
+		}
+	}
 }
